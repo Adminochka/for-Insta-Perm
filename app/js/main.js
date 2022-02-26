07f4ae2fb7c2f19943175d4f6143ddd67be39347
Maksar <adminochka@gmail.com>
2022-02-26 15:00:29 +0200

    model

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
 $(function (){
 
+    const modal = document.getElementById("js-model");
+    const btnOpen = document.getElementById("js-model-btn-open");
+    const btnClose = document.getElementById("js-model-btn-close");
+
+    btnOpen.addEventListener('click', function() {
+        modal.classList.add('active');
+    });
+
+    btnClose.addEventListener('click', function() {
+        modal.classList.remove('active');
+    });
+
+    document.addEventListener('keydown', function(e) {
+        if (e.key === 'Escape') {
+            modal.classList.remove('active');
+        }
+    });
+
+});
+
+$(".form-reg__select").select2({
+    minimumResultsForSearch: Infinity,
+    placeholder: 'Country',
 });
--- gulpfile.js
+++ gulpfile.js
@@ -53,6 +53,7 @@ function styles () {
 function scripts () {
     return src([
         'node_modules/jquery/dist/jquery.js',
+        'node_modules/select2/dist/js/select2.js',
         'app/js/**/main.js'
     ])
         .pipe(concat('main.min.js'))
